3ba869f38fd61308639606f87928c673154f1a1d
Aadi <aadishaktit@gmail.com>
2020-10-11 14:31:48 +0530

    Simple password generator is created using Django

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from generator import views
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path('',views.home,name='home'),
+    path('password',views.password),
+    path('about',views.about,name='about')
 ]
